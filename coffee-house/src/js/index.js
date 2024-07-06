9b9d16243616f58bd69e508832e91711829af00c
Ekaterina <114580132+besyashka@users.noreply.github.com>
2024-07-06 23:41:53 +1000

    Revert "Coffee house"

--- coffee-house/src/js/index.js
+++
@@ -1,8 +0,0 @@
-import { products } from './products.js';
-import { hamburgerMenuHandler } from './hamburger.js';
-import { initSlider } from './slider.js';
-
-window.onload = function () {
-  hamburgerMenuHandler();
-  initSlider();
-};
